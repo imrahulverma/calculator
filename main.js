e2acc3eac6b563223773cfdb7a6f6913cb5dbfc7
imrahulverma <rahul271096@gmail.com>
2022-12-14 17:47:16 +0530

    invalid expression

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 clk = document.querySelector(".buttons").addEventListener("click", calc);
-var disp=document.querySelector(".display")
+var disp = document.querySelector(".display");
 function calc() {
   on = event.target.id;
 
-  switch(on){
+  switch (on) {
     case "=":
-      if(disp.innerHTML==""){
-        disp.innerHTML=0
+      if (disp.innerHTML == "") {
+        disp.innerHTML = 0;
       }
-      try{
-        disp.innerHTML=eval(disp.innerHTML);
+      try {
+        disp.innerHTML = eval(disp.innerHTML);
+      } catch {
+        disp.innerHTML = "Invalid Expression";
       }
-      catch{
-        
-        disp.innerHTML="<span>Invalid Expression</span>"
+      break;
+    case "x":
+      if (disp.innerHTML == "Invalid Expression") {
+        disp.innerHTML = "";
+      } else {
+        disp.innerHTML = disp.innerHTML.slice(0, -1);
       }
-    break;
-    case "x":disp.innerHTML = disp.innerHTML.slice(0,-1);
-    break;
-    case "c":disp.innerHTML = "";
-    break;
+
+      break;
+    case "c":
+      disp.innerHTML = "";
+      break;
     default:
+      if (disp.innerHTML == "Invalid Expression") {
+        disp.innerHTML = on;
+      } else {
         disp.innerHTML += on;
+      }
   }
 }
